daeb720bddfbc3399ab76dc5a90ba5738c73060d
Patiphan Phengpao <misterpatipan123@gmail.com>
2021-07-09 14:04:09 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TableTop from 'tabletop';
+
+function App() {
+  
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    TableTop.init({
+    key: '1Py1bvpFxg2aBjxaOSRdHjza5YDQJrh9UHp3siflykyc',
+      callback: googleData => {
+        setData(googleData);
+      },
+      simpleSheet: true
+    })
+  }, []);
+
+  console.log('updated state --->', data)
+
+  return (
+
+    <div className="App">
+      <div id="employee-details">
+          {
+            data.map(obj => {
+              return (
+                <div key={obj.name}>
+                  <p>{obj.name}</p>
+                  <p>{obj.favDog}</p>
+                  <img alt={obj.favDog} src={obj.img} />
+                </div>
+              )
+            })
+          }
+        </div>
+    </div>
+  );
+}
+
+export default App;
